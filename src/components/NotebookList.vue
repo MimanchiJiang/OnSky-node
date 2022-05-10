869faa1762060a9c0f2f4ac0a62c023512104be1
MiManchi <2237268069@qq.com>
2022-05-10 18:35:36 +0800

    未登录时跳转到登录页面

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,3 +1,4 @@
+import Auth from '@/apis/auth';
 <template>
   <div id="notebook-list">
     <h1>{{ msg }}</h1>
@@ -9,12 +10,20 @@
 </template>
 
 <script>
+import Auth from "@/apis/auth";
 export default {
   name: "Login",
   data() {
     return {
       msg: "笔记本列表",
     };
+  },
+  created() {
+    Auth.getInfo().then((res) => {
+      if (!res.isLogin) {
+        this.$router.push({ path: "/login" });
+      }
+    });
   },
 };
 </script>
