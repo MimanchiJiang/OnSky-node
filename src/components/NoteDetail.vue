99e5ba63180ca55a587f08e8efbd843d029cba1e
MiManchi <2237268069@qq.com>
2022-05-12 21:43:56 +0800

    创建notesidebar，并创建note.js接口

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,13 +1,19 @@
 <template>
-  <div id="note-detail">
-    <h1>notebookId:{{ $route.query.notebookId }}</h1>
-    <h1>noteId:{{ $route.query.noteId }}</h1>
+  <div id="note" class="detail">
+    <note-sidebar></note-sidebar>
+
+    <div id="note-detail">
+      <h1>notebookId:{{ $route.query.notebookId }}</h1>
+      <h1>noteId:{{ $route.query.noteId }}</h1>
+    </div>
   </div>
 </template>
 
 <script>
 import Auth from "@/apis/auth";
+import NoteSidebar from "./NoteSidebar.vue";
 export default {
+  components: { NoteSidebar },
   name: "Login",
   data() {
     return {
